53297b5796b85cddab7471e468110e928a591d59
Mahendra Gohil <mpgohilse@gmail.com>
2025-06-13 16:48:38 +0530

    add contibution file

--- yearcommit.sh
+++ yearcommit.sh
@@ -1,33 +1,32 @@
 #!/bin/bash
 
-start_date=$(date -v-365d +%Y-%m-%d)
+# Config
+GIT_AUTHOR_NAME="Mahendra Gohil"
+GIT_COMMITTER_NAME="Mahendra Gohil"
+GIT_AUTHOR_EMAIL="mpgohilse@gmail.com"
+GIT_COMMITTER_EMAIL="mpgohilse@gmail.com"
 
-messages=(
-  "update readme: added dark mode screenshot"
-  "update readme: added schema design of aggregation"
-  "update readme: changed feature description"
-  "update readme: changed ENV description"
-)
+touch .contrib.txt
 
-for i in {0..4}; do
-  commit_date=$(date -v+${i}d -j -f "%Y-%m-%d" "$start_date" +%Y-%m-%d)
+for i in {0..364}
+do
+  # Get date i days ago in required format
+  day=$(date -v -${i}d "+%Y-%m-%d")
 
-  echo "Committing for date: $commit_date"
+  for j in {0..2}
+  do
+    # Use different times for each commit (ensures uniqueness)
+    hour=$((10 + j))
+    timestamp="${day}T${hour}:00:00 +0530"
 
-  for j in {0..2}; do
-    msg_index=$(((i * 3 + j) % ${#messages[@]}))
-    commit_msg="${messages[$msg_index]}"
+    echo "$timestamp commit $j" >> .contrib.txt
+    git add .contrib.txt
 
-    echo "<!-- schema update $commit_date $j -->" >> README.md
-
-    git add README.md
-
-    hour="12"
-    minute="0$j"
-    timestamp="${commit_date}T${hour}:0$j:00"
-
-    GIT_AUTHOR_DATE="$timestamp" GIT_COMMITTER_DATE="$timestamp" git commit -m "$commit_msg"
+    GIT_AUTHOR_DATE="$timestamp" \
+    GIT_COMMITTER_DATE="$timestamp" \
+    git commit -m "daily commit on $day - $j"
   done
 done
 
+# Push all commits
 git push origin main
